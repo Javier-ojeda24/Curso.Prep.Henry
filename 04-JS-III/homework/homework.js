91f4c3857368649dab4078f30d993813331614b8
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2022-04-18 20:07:49 -0300

    resolucion tarea

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -68,16 +68,15 @@ function arrayContiene(array, elemento) {
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
   for(let i = 0; i < array.length; i++){
-    if(array[i]=== elemento){
-      return true;
-    }
-  }return false;
+    if(array[i]=== elemento) return true;
+  }
+  return false;
 }
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-  let suma =[];
+  let suma = 0;
   for(let i =0 ; i < numeros.length ;i ++){
     suma += numeros[i];
   }
@@ -89,9 +88,9 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  acumulador =0;
+  acumulador = 0;
   for (let i = 0 ; i < resultadosTest.length;i++){
-   acumulador += resultadosTest[i]; 
+  acumulador += resultadosTest[i]; 
   }
   return acumulador / resultadosTest.length;
 }
@@ -101,12 +100,12 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  let caja = 0;
+  let masGrande = 0;
   for (let i = 0; i < numeros.length;i++){
-    if(numeros[i]> caja){
-      caja = numero[i];
+    if(numeros[i]> masGrande){
+    masGrande= numero[i];
     }
-  }return caja;
+  }return masGrande;
 }
 
 
@@ -119,13 +118,18 @@ function multiplicarArgumentos() {
   }else if (arguments.length === 1){
     return arguments [0];
   }
+  let producto = 1;
+  for(let i =0; i < arguments.length;i++){
+    producto *= arguments[i];
+  }
+  return producto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  let mayores = [];
+  let mayores = 0;
   for(let i = 0 ; i < arreglo.length;i ++){
     if(arreglo[i] > 18){
       mayores.push(arreglo[i]);
@@ -150,46 +154,57 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  for(let i = 0; i< arreglo.length -1; i++){
-    if(arreglo[i]!== arreglo[i +1]){
+  for(let i = 1; i< arreglo.length; i++){
+    if(arreglo[0]!== arreglo[i]){
       return false;
-    }
-  }
-
+    } 
+  }return true;
+}
 
 function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let nuevoArrys = 0;
+  for(let i=0; i <array.length;i++){
+    if(
+      array[i]=== "Enero";
+      array[i]=== ""
+    )
+  }
 }
-
-
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  arrayMayor = 0;
+  for(let i =0;i<array.length;i++){
+    if(array[i]>100){
+      arrayMayor.push(array[i])
+    }
+  }
+  return arrayMayor;
 }
 
 
-function breakStatement(numero) {
-  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
-  //Guardar cada nuevo valor en un array. 
-  //Devolver el array
-  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
-  //devolver: "Se interrumpió la ejecución"
-  //Pista: usá el statement 'break'
-  // Tu código:
-}
-
-
-function continueStatement(numero) {
+//
+  function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
   //Devolver el array
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let arrayValores =0;
+  for(letr i=0;i<10;i++){
+    if(i===5){
+      continue
+    }
+    else{numero=numero+2;
+    arrayValores.push(numero);
+    }
+  }return arrayValores;
 }
 
 
